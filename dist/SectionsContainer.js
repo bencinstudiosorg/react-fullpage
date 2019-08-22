8f87aab4022a6d57aceb66204629d7b33d7697bb
james-carroll-bencin <james.carroll@bencinstudios.com>
2019-08-22 12:07:57 -0500

    fix(nav): fix nav anchor active with new hrefs

--- dist/SectionsContainer.js
+++ dist/SectionsContainer.js
@@ -54,17 +54,14 @@ var SectionsContainer = function (_Component) {
     }, _this.addActiveClass = function () {
       _this.removeActiveClass();
 
-      var hash = window.location.hash.substring(1);
-        var activeLinks = document.querySelectorAll(hash.includes("#") ? 'a[href="' + hash + '"]' : 'a[href="#' + hash + '"]');
+      var hash = window.location.hash;
+        var activeLinks = document.querySelectorAll('a[href="' + window.location.pathname + hash + '"]');
 
       for (var i = 0; i < activeLinks.length; i++) {
         activeLinks[i].className = activeLinks[i].className + (activeLinks[i].className.length > 0 ? ' ' : '') + ('' + _this.props.activeClass);
       }
     }, _this.removeActiveClass = function () {
-        var activeLinks = document.querySelectorAll(_this.props.anchors[_this.state.activeSection] != null
-            && _this.props.anchors[_this.state.activeSection].includes("#")
-            ? 'a:not([href="' + _this.props.anchors[_this.state.activeSection] + '"])'
-            : 'a:not([href="#' + _this.props.anchors[_this.state.activeSection] + '"])');
+        var activeLinks = document.querySelectorAll('a:not([href="#' + _this.props.anchors[_this.state.activeSection] + '"])');
 
       for (var i = 0; i < activeLinks.length; i++) {
         activeLinks[i].className = activeLinks[i].className.replace(/\b ?active/g, '');
@@ -190,7 +187,7 @@ var SectionsContainer = function (_Component) {
             var hash = that.props.anchors[direction];
 
             if (!that.props.anchors.length || hash) {
-              window.location.hash = hash.includes('#') ? hash : '#' + hash;
+              window.location.hash = '#' + hash;
             }
 
             that.handleSectionTransition(direction);
@@ -210,7 +207,7 @@ var SectionsContainer = function (_Component) {
       var hash = _this.props.anchors[index];
 
       if (!_this.props.anchors.length || hash) {
-        window.location.hash = hash.includes("#") ? hash : '#' + hash;
+        window.location.hash = '#' + hash;
       }
     }, _this.handleScrollCallback = function () {
       if (_this.props.scrollCallback) {
@@ -251,7 +248,7 @@ var SectionsContainer = function (_Component) {
         };
 
         return _react2.default.createElement('a', {
-          href: window.location.pathname + (link.includes("#") ? link : '#' + link),
+          href: window.location.pathname + '#' + link,
           key: index,
           className: _this.props.navigationAnchorClass || 'Navigation-Anchor',
           style: _this.props.navigationAnchorClass ? null : anchorStyle
